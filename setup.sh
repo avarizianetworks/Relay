1fe6ab2fe807baac97327a0b8de03850b0329f63
Bjarne Matz <account@bjarnematz.de>
2024-09-20 01:25:44 +0200

    Relay Listner auf neue IP beschränken Fixes #2

--- setup.sh
+++ setup.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 target=home.avarizia.eu
-/usr/bin/6tunnel 2000 $target 2000 # Minecraft Velocity Proxy
-/usr/bin/6tunnel 80 $target 80 # HTTP
-/usr/bin/6tunnel 443 $target 443 # HTTPS 
+source=207.180.222.47
+/usr/bin/6tunnel -l $source 2000 $target 2000 # Minecraft Velocity Proxy
+/usr/bin/6tunnel -l $source 80 $target 80 # HTTP
+/usr/bin/6tunnel -l $source 443 $target 443 # HTTPS  
